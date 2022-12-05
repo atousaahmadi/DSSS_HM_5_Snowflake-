44fc0920e29f7c6b5688489be14b5833f80e089c
Atousa Ahmadi <atousaahmadi@Atousas-MacBook-Air.local>
2022-12-05 01:18:12 +0100

    .py: setup is made

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'snowflake' ,
+    description='make colorful snowflake :)',
+    version = '0.0.1',
+    author= 'atousaahmadi',
+    author_email= 'atoosaahmadi01@gmail.com',
+    license= 'Apache 2.0',
+    packages = find_packages(),
+)
